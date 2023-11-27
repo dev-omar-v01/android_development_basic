6727cec5e35a83cb53ca72e9dfb87b20b89df6ce
softdevomar <omorfarukqqq@gmail.com>
2023-11-27 05:27:07 -0800

    change test java code

--- app/src/main/java/com/example/myfirarapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirarapplication/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.myfirarapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+     private TextView nameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameTextView = (TextView) findViewById(R.id.TextId1);
+        nameTextView.setText("Tanvir");
     }
 }
